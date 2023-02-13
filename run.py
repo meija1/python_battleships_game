edc3018813589a14608af775b3edc1486ccc4e67
meija1 <meija911@gmail.com>
2023-02-13 17:57:41 +0000

    Create basic game boards and import randint class

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+"""
+Create base for 8 by 8 boards for player, computer 
+and a hidden overlay one for computer.
+"""
+player_game_board = [[" "] * 8 for _ in range(8)]
+computer_game_board = [[" "] * 8 for _ in range(8)]
+hidden_board = [[" "] * 8 for _ in range(8)]
