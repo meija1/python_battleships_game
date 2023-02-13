f8fc110d45932116ae5e307bf656d9d954c563d9
meija1 <meija911@gmail.com>
2023-02-13 18:23:05 +0000

    Add print board function to create gameplay area

--- run.py
+++ run.py
@@ -7,3 +7,17 @@ and a hidden overlay one for computer.
 player_game_board = [[" "] * 8 for _ in range(8)]
 computer_game_board = [[" "] * 8 for _ in range(8)]
 hidden_board = [[" "] * 8 for _ in range(8)]
+
+"""
+Create a function to style the game board by passing a variable board
+Print numbers horizontally and vertically
+Finally print vertical lines and implement rows by 1
+"""
+def print_board(board):
+    print('  1 2 3 4 5 6 7 8')
+    row_num = 1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num += 1
+
+
